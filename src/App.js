5f2a678ca4bbb8b1eef911f2d4010c74b79c6f1f
Sandeep <sandeep.b@codebewinnovations.com>
2023-10-31 11:54:24 +0530

    props

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
-
-import Add from "./components/Add";
-import Grretings from "./components/Grretings";
+import React, { useState } from 'react';
+import Web3 from 'web3';
+import { OpenSeaSDK } from 'opensea-js';
+ 
 
 function App() {
+  console.log(OpenSeaSDK,"getNFTsByAccount");
+
   return (
     <div>
-      <Add/>
-      <Grretings/>
+      <button >Connect to MetaMask</button>
     </div>
   );
 }
 
 export default App;
- 
